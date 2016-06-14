2fa9dd169d789751c0a174fb439bab003107a5f4
hsmaniee <hs.bibak0098@gmail.com>
2016-06-14 04:03:37 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {hs989898},--Sudo users
+    sudo_users = {128283383},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
